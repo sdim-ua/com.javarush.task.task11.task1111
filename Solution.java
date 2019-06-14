b81f10da5b07ccead51b6d2ae85deea9747802e8
sdim-ua <ingersol@yandex.ua>
2019-06-14 09:54:06 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,18 @@
+/* 
+«Из ребра его»
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    //Адам
+    public class Adam {
+
+    }
+
+    //Ева
+    public class Eve extends Adam {
+
+    }
+}
